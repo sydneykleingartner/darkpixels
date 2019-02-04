d5e47155a039298150e0ae599074c94ad4048eba
Sydney Kleingartner <43350106+sydneykleingartner@users.noreply.github.com>
2019-02-04 18:54:15 -0500

    start of code for pixels
    did work on initializing methods

---
+++ step1.py
@@ -0,0 +1,20 @@
+#python program that loads image and prints it
+
+def main ():
+
+
+if __name__ == '__main__':
+	main()
+
+#extract all pixel values from the image
+
+
+#store all the pixels in a two dimensional array
+#each pixel is a three dimensional array on its own
+
+
+
+#for loop through the array
+#goal: to find the darkest pixel
+
+
