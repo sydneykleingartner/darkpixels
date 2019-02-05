082fe00311da1ceae654af71b547fc7cfb934d23
Sydney Kleingartner <skleingartner@gwu.edu>
2019-02-04 19:22:52 -0500

    added image module, created image object, opened image

--- step1.py
+++ step1.py
@@ -1,7 +1,18 @@
-#python program that loads image and prints it
+#step one of the project
+#goal: python program that loads image and prints it
+
+#>>>from PIL import Image
+
+#>>>im = Image.open('grace-hopper.png', ' r')
 
 def main ():
+	
+	#importing the Image module of PIl
+	from PIL import Image
 
+	#creating an Image object
+	#opening the image for reading mode (so then we can do stuff with it!)
+	im = Image.open('grace-hopper.png', 'r')
 
 if __name__ == '__main__':
 	main()
