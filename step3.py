bf07ac61da419a2ec1a06e04fa1aa9147ffa69f6
Sydney Kleingartner <skleingartner@gwu.edu>
2019-02-09 20:19:06 -0500

    adding process work done during this week on how to approach storing/accessing the darkest pixels

--- step3.py
+++ step3.py
@@ -34,24 +34,57 @@ def main ():
 	for x in range(len(pixel_values)):
 		print pixel_values[x],	
 
-	#STEP 3
-
+	#STEP THREE
 	#sum variable adds up the RGB values of each pixel
 	sum = 0	
 
 	#comparison variable against sum
-	smallest_and_darkest = 0;
+	#assign to the first pixel's sum
+	smallest_and_darkest = pixel_values[0][0][0] + pixel_values[0][0][1] + pixel_values[0][0][2]
 
 	#list of the two darkest pixels
 	darkest_pixels = []
+	#assign darkest pixel (the first position in this list) to the first pixel in the image
+	darkest_pixels[0] = pixel_values[0][0]	
+	
+	#!!!!!
+	#new strategy
+	#create a list of the sums of the three values of each pixel in the pixel_values list
+	sums = []
+	#for loop to add in the elements
+	for k in range(len(pixel_values)):
+		sums[k] = pixel_values[k][0] + pixel_values[k][1] + pixel_values[k][2]
 
+	#find the minimum value in the sums list
+	#store minimum value in darkest_pixels
+		#we also need to store which pixel that was -> currently storing the sum
+		#i don't know what to do here
+	darkest_pixels.extend(min(sums))
+
+	#remove min
+	#find the min again
+	#store second min in darkest_pixels
+	
+	#how to make sure that the two pixels aren't next to each other??
+
+	#OLD STRATEGY
+	#actually how are these arrays stored is it by line or the whole thing is one long one dimensional array (the 2D elements are the 3 values)
 	#for loop through the three dimensions already stored for each pixel
 	#compare values using the sum variable
 	for i in range(len(pixel_values)):
 		for j in range(len(pixel_values)):
-			for k in range(len(pixel_values)):
-				#sum is equal to the three values for that pixel added together
-				sum = pixel_values[i][j][k]
+			#sum is equal to the three values for that pixel added together
+			sum = pixel_values[i][j][0] + pixel_values[i][j][1] + pixel_values[i][j][2]
+			#if this sum is smaller than the current smallest (meaning darkest pixel)
+			if sum < smallest_and_darkest:
+				#reassign smallest_and_darkest to the sum at that position
+				smallest_and_darkest = sum
 
+	#once we have found the darkest pixel and stored it in the darkest_pixels
+	#we want to go find the second darkest
+	#for m in range(len(pixel_values)):
+		#for n in range(len(pixel_values)): 
+				
+		
 if __name__ == '__main__':
 	main ()
